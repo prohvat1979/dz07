57400a63a61d4d51d822484e899db5cd77a9b71a
«Sergey» <notan1979@yandex.ru>
2023-07-04 21:36:47 +0300

    first commit

---
+++ z47/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int ReadInt(string text) // ввод
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillMatrix(double[,] array) //функция заполнения массива вещественными числами
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+}
+
+void PrintMatrix(double[,] matrixPrint) // функция вывода массива в терминал
+{
+    for (int i = 0; i < matrixPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+// -----------------
+
+int rows = ReadInt("Введите количество строк: ");
+int cols = ReadInt("Ведите количество столбцов: ");
+double[,] matrix = new double[rows, cols];
+FillMatrix(matrix);
+PrintMatrix(matrix);
+
---
+++ z52/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+// в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadInt(string text) // ввод
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int rows = ReadInt("Введите количество строк: ");
+int cols = ReadInt("Введите количество столбцов: ");
+int[,] matrix = new int[rows, cols];
+FillMatrix(matrix);
+PrintMatrix(matrix);
+
+double[] Matrix = new double[matrix.GetLength(1)];
+
+for (int i = 0; i < matrix.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        result = result + matrix[j, i];
+    }
+    Matrix[i] = result / matrix.GetLength(0);
+}
+PrintArray(Matrix);
+
+
+// функция заполнения массива  вещественными числами
+void FillMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+//  функция вывода двухмерного массива в терминал
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// функция вывода массива в терминал 
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
